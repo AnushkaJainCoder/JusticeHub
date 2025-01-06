945818a592590817357bb61602675c0425af3904
anushka.jain <anushka.jain@innovasolutions.com>
2025-01-06 16:22:19 +0530

    create header component

---
+++ src/Components/Header.js
@@ -0,0 +1,23 @@
+export function Header() {
+    return (
+        <>
+            <h1>JusticeHub</h1>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="/">Home</a>
+                    </li>
+                    <li>
+                        <a href="/services">Services</a>
+                    </li>
+                    <li>
+                        <a href="/about">About</a>
+                    </li>
+                    <li>
+                        <a href="/contact">Contact</a>
+                    </li>
+                </ul>
+            </nav>
+        </>
+    )
+}
