43f3059a9662035cb3dd1b5a71d0fd2222efe953
anushka.jain <anushka.jain@innovasolutions.com>
2025-01-06 17:04:33 +0530

    add contact page

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,6 +1,8 @@
-export function About(){
-    return(
+export function About() {
+    return (
         <>
+            <h2>About</h2>
+            <p> our law has firOur law firm has been serving the community for over 20 years...</p>
         </>
     )
 }
