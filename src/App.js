431e73831c2aa479f326fc47a0d2d13e75a8619a
anushka.jain <anushka.jain@innovasolutions.com>
2025-01-06 16:50:27 +0530

    adding routes in nav bar

--- src/App.js
+++ src/App.js
@@ -2,12 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 import { Header } from './Components/Header';
 import { Footer } from './Components/Footer';
+import { Route, Routes } from 'react-router-dom';
+import { Home } from './Pages/Home';
+import { About } from './Pages/About';
+import { Services } from './Pages/Services';
+import { Contact } from './Pages/Contact';
 
 function App() {
   return (
     <div className="App">
-     <Header />
-     <Footer />
+      <Routes >
+        <Route path='/' element={<Home />} />
+        <Route path='/services' element={<Services />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
+      <Header />
+      <Footer />
     </div>
   );
 }
